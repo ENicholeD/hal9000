b96a2ab3f13b889c93677c2e88166c049021b160
Erin Deuel <end.gaia@gmail.com>
2019-08-23 09:28:57 -0700

    add javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
 (document).ready(function(){
   (".form").submit(function(event){
     event.preventDefault();
+
     var userInput = $("#numberInput").val();
+    var beeps = [];
+
+    for (i = 0; i <= userInput; i++){
+      beeps.push(i.toString());
+    }
+
+    output = boops(beeps, userInput){
+
+      for (beeps[i].includes("3")){
+        beeps[i] = "Sorry Dave. I can't do that."
+      }
+      if (beeps[i].includes("2")){
+
+      }
+    }
   });
 });
