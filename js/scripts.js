b2d1008c3f9aad63856f76b0f92a13e8c47c7de0
Erin Deuel <end.gaia@gmail.com>
2019-08-23 11:00:39 -0700

    add some CSS styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-(document).ready(function(){
-  (".form").submit(function(event){
+$(document).ready(function(){
+  $(".form").submit(function(event){
     event.preventDefault();
 
     var userInput = $("#numberInput").val();
@@ -9,21 +9,23 @@
       beeps.push(i.toString());
     }
 
-    output = boops(beeps, userInput){
-      $("#out").text(output);
-    }
+    output = boops(beeps, userInput);
+    $(".col-md-5").fadeIn();
+    $(".form").hide();
+    $("#out").text(output);
 
-      function boops(beeps, userInput){
-        for (i = 0; i <= userInput; i++){
-          if (beeps[i].includes("3")){
-            beeps[i] = "Sorry Dave. I can't do that.";
-          } else if (beeps[i].includes("2")){
-            beeps[i] = "Boop";
-          } else if (beeps[i].includes("1")){
-            beeps[i] = "Beep";
-          }
+
+    function boops(beeps, userInput){
+      for (i = 0; i <= userInput; i++){
+        if (beeps[i].includes("3")){
+          beeps[i] = "Sorry Dave. I can't do that";
+        } else if (beeps[i].includes("2")){
+          beeps[i] = "Boop";
+        } else if (beeps[i].includes("1")){
+          beeps[i] = "Beep";
         }
-        return beeps;
       }
-    });
+      return beeps;
+    }
   });
+});
